048b9ebd3296f866323df89cee5d89a0b8dcdccc
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-11 09:48:09 +0900

    chore: bump SpeedLab to 0.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.e2bspeedlab"
         minSdk = 31
         targetSdk = 37
-        versionCode = 19
-        versionName = "0.3.3"
+        versionCode = 20
+        versionName = "0.3.4"
 
         ndk {
             abiFilters += "arm64-v8a"
